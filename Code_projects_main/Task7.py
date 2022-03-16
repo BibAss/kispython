22be9c55ae4ea232575bad1fa3bffa55a5bb3980
IVC1-5 <byba003@mail.ru>
2022-03-16 19:27:49 +0300

    add 7

--- Code_projects_main/Task7.py
+++ Code_projects_main/Task7.py
@@ -1,31 +1,29 @@
-def f(x):
+def main(n):
     d = 0x00000000
-    temp = x & 0x0000000f  # A
+    x = hex(n)
+    temp = int(x, 16) & 0x0000000f  # A
     temp = temp << 28
     d = d | temp
-    temp = x & 0x00100000  # C
+    temp = int(x, 16) & 0x00100000  # C
     temp = temp << 7
     d = d | temp
-    temp = x & 0xf0000000  # H
+    temp = int(x, 16) & 0xf0000000  # H
     temp = temp >> 5
     temp = temp & 0x07800000
     d = d | temp
-    temp = x & 0x04000000  # F
+    temp = int(x, 16) & 0x04000000  # F
     temp = temp >> 4
     d = d | temp
-    temp = x & 0x03800000  # E
+    temp = int(x, 16) & 0x03800000  # E
     temp = temp >> 4
     d = d | temp
-    temp = x & 0x08000000  # G
+    temp = int(x, 16) & 0x08000000  # G
     temp = temp >> 9
     d = d | temp
-    temp = x & 0x000ffff0  # B
+    temp = int(x, 16) & 0x000ffff0  # B
     temp = temp >> 2
     d = d | temp
-    temp = x & 0x00600000  # D
+    temp = int(x, 16) & 0x00600000  # D
     temp = temp >> 21
     d = d | temp
-    return d
-
-
-print(f(0x8854acb9))
+    return int(hex(d), 16)
