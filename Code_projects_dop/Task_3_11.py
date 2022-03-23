7ee3f7f55b54918d4e955afa48868216f70b64f3
IVC1-5 <byba003@mail.ru>
2022-03-23 19:05:25 +0300

    add task 11 in 3 pract

---
+++ Code_projects_dop/Task_3_11.py
@@ -0,0 +1,34 @@
+import re
+import matplotlib.pyplot as plt
+
+
+def get_src():
+    f = open('src.txt', 'r', encoding='utf-8')
+    res = f.read()
+    src = re.findall(r';"\d+"', res)
+    for i in range(len(src)):
+        src[i] = src[i][2:-1]
+    src = tuple(sorted(src))
+    return src
+
+
+def get_counters(src):
+    b = {}
+    c = 1
+    for i in range(len(src) - 1):
+        if src[i] == src[i + 1]:
+            c += 1
+        else:
+            b[src[i]] = c
+            c = 1
+    return b
+
+
+def draw_plot(b):
+    names = list(b.keys())
+    amount = list(b.values())
+    plt.bar(names, amount)
+    plt.show()
+
+
+draw_plot(get_counters(get_src()))
