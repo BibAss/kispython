a8a802e88b5bb9c4c2d0c759420bebe03e57f0d2
IVC1-5 <byba003@mail.ru>
2022-03-23 19:24:14 +0300

    add task 11 in 3 pract

--- Code_projects_dop/Task_3_11.py
+++ Code_projects_dop/Task_3_11.py
@@ -12,6 +12,16 @@ def get_src():
     return src
 
 
+def get_srce():
+    f = open('src.txt', 'r', encoding='utf-8')
+    res = f.read()
+    srce = re.findall(r';"\w+";', res)
+    for i in range(len(srce)):
+        srce[i] = srce[i][2:-2]
+    srce = tuple(sorted(srce))
+    return srce
+
+
 def get_counters(src):
     b = {}
     c = 1
@@ -24,11 +34,29 @@ def get_counters(src):
     return b
 
 
-def draw_plot(b):
+def get_counters_genre(src):
+    b = {}
+    c = 1
+    for i in range(len(src) - 1):
+        if src[i] == src[i + 1]:
+            c += 1
+        else:
+            b[src[i]] = c
+            c = 1
+    return b
+
+
+def draw_plot_bar(b):
     names = list(b.keys())
     amount = list(b.values())
     plt.bar(names, amount)
     plt.show()
 
 
-draw_plot(get_counters(get_src()))
+def draw_plot_pie(b):
+    names = list(b.keys())
+    year = list(b.values())
+    plt.pie(year, labels=names)
+    plt.show()
+
+draw_plot_pie(get_counters_genre(get_srce()))
